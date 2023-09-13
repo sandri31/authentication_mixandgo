e7cdb327f5268ff1a48d0ac12d45201f5ef5e121
Florent S <sandri822@hotmail.fr>
2023-09-13 20:11:17 +0200

    ✨ Create MVC User for authentication

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  get 'dashboards/index', as: :dashboards
+  resources :logins, only: %i[new create]
+  resources :registrations, only: %i[new create]
 end
